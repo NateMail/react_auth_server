729493666d3c6fdc9ba5774538fca38bf823715a
nathanmailhiot <nathanjm12@outlook.com>
2019-10-06 14:08:05 -0400

    install jwt

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,11 @@
 const User = require('../models/user');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function(req, res, next) {
   const email = req.body.email;
@@ -31,7 +38,7 @@ exports.signup = function(req, res, next) {
         return next(err);
       }
       // Respond to request with indicating the user was created
-      res.json({ success: true });
+      res.json({ token: tokenForUser(user) });
     });
   });
 };

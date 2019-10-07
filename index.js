2ad80c07e8bad7abac26084978d06b397d5decb3
nathanmailhiot <nathanjm12@outlook.com>
2019-10-07 09:32:50 -0400

    Npm install cors and allow for requests

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // DB Setup
 mongoose.connect('mongodb://localhost/auth', {
@@ -20,6 +21,7 @@ connection.on('connected', function() {
 
 // App Setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
